88fd1eeb7caa7b5689a276ed5ce88cc47fb088d5
SyedUmair1998 <syedumairh1998@gmail.com>
2021-10-14 19:36:29 +0500

    first commit

--- App.js
+++ App.js
@@ -1,71 +1,49 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import MapView,{Polygon, PROVIDER_GOOGLE, Circle, Heatmap} from 'react-native-maps';
-import { Marker, Callout } from 'react-native-maps';
+import React, {useState} from 'react';
+import {View, Text,StyleSheet, Button , Image} from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 
 const App =  () => {
+
+
+  const [image,setImage] = useState(null);
+  const handleClick = () => {
+    const options = {
+      title:'Select image'
+    };
+    
+  launchImageLibrary(options,(res)=>{
+    if(res.didCancel)
+    {
+      return console.log("Cancel")
+    }
+    console.log(res.assets[0].uri);
+    setImage(res.assets[0].uri);
+  })
+  };
+  
 return(
-  <View> 
+<View style={styles.main}>
+ {
+   image ? <Image style={{width:100,height:100}} source = {{uri :image}} /> :  <Button title="Gallery" onPress={handleClick} />
+ }
 
-<MapView
-style = {{height:'100%'}}
-  provider={PROVIDER_GOOGLE}
-region = {{
-  latitude:24.860966,
-  longitude:66.990501,
-  latitudeDelta:0.09,
-  longitudeDelta:0.03,
 
-}}>
-  {/* <Polygon
-  coordinates={[
-    { latitude:24.860966 , longitude:66.980501 },
-    { latitude:24.810966 , longitude:66.970501 },
-    { latitude:24.800966 , longitude:66.960501 },
-    { latitude:24.790966,  longitude:66.940501 }
-    ]}
-      fillColor={'rgba(100,200,200,0.6)'}
-   />
-   <Circle
-   center =  {{
-    latitude:24.860966 , longitude:66.980501
-   }}
-   radius={1000}
-   fillColor={'rgba(200,300,200,0.5)'}
-   />
-   
-  <Marker
-  coordinate=
-  {{ latitude:24.860966,
-    longitude:66.990501
-  }}
-  >
-    <Callout>
-      <Text style={{color:'red',backgroundColor:'yellow'}}>Karachi</Text>
-    </Callout>
-
-  </Marker> */}
-<Heatmap 
-points={[
-  { latitude:24.840966 , longitude:66.980501 },
-  { latitude:24.830966 , longitude:66.970501 },
-  { latitude:24.820966 , longitude:66.960501 },
-  { latitude:24.810966,  longitude:66.940501 }
-  ]} 
-  radius={40}
-  gradient={{
-    colors:["black","purple","red","yellow","white"],
-    startPoints:[0.2, 0.4, 0.6, 0.8, 0.9],
-  }}
-  >
-
-</Heatmap>
-</MapView>
 </View>
 
 
 )}
+const styles = StyleSheet.create({
+  main : {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'white'
+  },
+  txt : {
+    color:'black'
+  }
+})
 
 
 
